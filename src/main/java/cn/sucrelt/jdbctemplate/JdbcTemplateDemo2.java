86b0547425a6372e5a5d1f6112cfe4030683776e
tangtangsama <1462339680@qq.com>
2020-10-14 10:50:14 +0800

    基本JDBC操作以及使用Spring实现JDBC操作

---
+++ src/main/java/cn/sucrelt/jdbctemplate/JdbcTemplateDemo2.java
@@ -0,0 +1,15 @@
+package cn.sucrelt.jdbctemplate;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcTemplateDemo2 {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+
+        jdbcTemplate.execute("insert into account(name,money)values('ddd',1000)");
+
+    }
+}
